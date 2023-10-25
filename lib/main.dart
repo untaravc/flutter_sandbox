e4ecdff2df3c93dc8c3f47754d25a532033a813f
Untara Vivi Chahya <vyvy1777@gmail.com>
2023-10-26 00:33:04 +0700

    extract widget, image, text, padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/sandbox/add_bloc_consumer.dart';
+import 'package:flutter_application_1/pages/tutorial/widget_dasar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AddBlocConsumer(),
+      home: WidgetDasar(),
     );
   }
 }
